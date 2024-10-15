b89d1b3bb184b0a5d666dc31f8695cd4af42b839
anas <anashafeez819@gmail.com>
2024-10-15 08:16:10 -0400

    my 3rd commit for noor bhai

---
+++ script3.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/env  bats
+# Function to check if a number is an Armstrong number
+is_armstrong() {
+    local number=$1
+    local num_digits=${#number}   # Get the number of digits
+    local sum=0
+    local temp=$number
+    # Loop through each digit
+    while [ "$temp" -gt 0 ]; do
+        digit=$(( temp % 10 ))    # Get the last digit
+        sum=$(( sum + digit**num_digits ))  # Add the digit raised to the power of the number of digits
+        temp=$(( temp / 10 ))     # Remove the last digit
+    done
+    # Check if the sum is equal to the original number
+    if [ "$sum" -eq "$number" ]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+# Input number (pass as a command line argument)
+is_armstrong "$1"
+#my 2nd script
